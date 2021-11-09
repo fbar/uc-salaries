6804aabfdc814f975d7a2e0359de17b521c1707f
François Bar <fbar@usc.edu>
2021-11-09 09:25:06 -0800

    script to download uc payroll as csv file

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # uc-salaries
 script to download uc payroll as csv file
+
+adapted from https://github.com/jk3587/ucop_comp per  https://www.reddit.com/r/datasets/comments/kn8n4r/looking_for_uc_school_salary_data_in_csv_format/ghjo01q/?utm_source=reddit&utm_medium=web2x&context=3
---
+++ ucsalaries.py
@@ -0,0 +1,34 @@
+import requests
+import csv
+  
+data = {
+  '_search': 'false',
+  'nd': '',
+  'rows': '315668',
+  'page': '1',
+  'sidx': 'EAW_LST_NAM',
+  'sord': 'asc',
+  'year': '2020',
+  'location': 'ALL',
+  'firstname': '',
+  'lastname': '',
+  'title': '',
+  'startSal': '0',
+  'endSal': '9999999'
+}
+
+response = requests.post('https://ucannualwage.ucop.edu/wage/search.action', data=data)
+
+content = response.content.decode('UTF-8')
+content_obj = eval(content)
+rows = content_obj['rows']
+
+fieldnames = ['id', 'year', 'location', 'firstname', 'lastname', 'title', 'gross-pay', 'regular-pay', 'overtime-pay', 'other-pay' ] 
+
+csv_file = "ucla-salaries.csv"
+
+with open(csv_file, 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(fieldnames) 
+    for data in rows:
+        writer.writerow(data['cell'])
